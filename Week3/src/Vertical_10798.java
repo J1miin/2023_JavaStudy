303479bceb830170456918f1e86ba6ee67556cdb
J1miin <gift040404@naver.com>
2023-10-04 17:55:55 +0900

    Update Vertical_10798.java
    수정

--- Week3/src/Vertical_10798.java
+++ Week3/src/Vertical_10798.java
@@ -5,12 +5,16 @@ public class Vertical_10798 {
     public static void main(String[] args) {
         final int ROW = 5;
         final int COL = 15;
+        String input;
         char sArray[][] = new char[ROW][COL]; // 2차원 배열 선언
         Scanner scanner = new Scanner(System.in);
         int i, j;
         System.out.println("입력하세요");
         for (i = 0; i < ROW; ++i) { // 2차원 배열에 값 입력받기
-            //여기 잘 모르겠...
+            input=scanner.next();
+            for (j=0;j<input.length();++j) {
+            	sArray[i][j]=input.charAt(j); //문자열 배열의 j번째 
+            }
         }
         for (i = 0; i < COL; ++i) { // 2차원 배열을 세로로 출력
             for (j = 0; j < ROW; ++j) {
